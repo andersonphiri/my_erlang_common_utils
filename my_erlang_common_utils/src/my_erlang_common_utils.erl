7b4d5ee466e349934ffb0df24ad2bfec2d524ef0
andersonphiri <phirianderson23@gmail.com>
2024-07-21 23:39:51 +0800

    add test method test with 0 arity

---
+++ my_erlang_common_utils/src/my_erlang_common_utils.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%%% @author chikuse2
+%%% @copyright (C) 2024, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 21. Jul 2024 11:16 PM
+%%%-------------------------------------------------------------------
+-module(my_erlang_common_utils).
+-author("chikuse2").
+
+%% API
+-export([start/0]).
+
+start() ->
+  io:format("Hello my name is my_erlang_common_utils_sup, created by anderson! ~n").
+
+%% pp 428
