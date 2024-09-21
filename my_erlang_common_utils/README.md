64396a6c6a8b2ce3ffe1e140cab81b57e79f7345
andersonphiri <phirianderson23@gmail.com>
2024-09-21 17:37:22 +0800

    add the second line

---
+++ my_erlang_common_utils/README.md
@@ -0,0 +1,9 @@
+my_erlang_common_utils
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
--- my_erlang_common_utils/src/my_erlang_common_utils.erl
+++ my_erlang_common_utils/src/my_erlang_common_utils.erl
@@ -13,6 +13,7 @@
 -export([start/0]).
 
 start() ->
-  io:format("Hello my name is my_erlang_common_utils_sup, created by anderson! ~n").
+  io:format("Hello my name is my_erlang_common_utils_sup, created by anderson! ~n"),
+  io:format("Hello my name is my_erlang_common_utils_sup, created by anderson, again! ~n").
 
 %% pp 428
